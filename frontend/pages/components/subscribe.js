be4518c46a43ed18e328cd981d067be216e77db0
venkatesh <venkatesh1754673niz@gmail.com>
2025-06-17 21:34:28 +0530

    minor changes

---
+++ frontend/pages/components/subscribe.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export function InputWithButton() {
+  return (
+    <div className="flex w-full max-w-sm items-center gap-2">
+      <Input type="email" placeholder="Email" />
+      <Button type="submit" variant="outline">
+        Subscribe
+      </Button>
+    </div>
+  );
+}
